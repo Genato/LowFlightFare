d7829a8290df7860c1e5fcbda70d539e741ceb88
Genato <druidhr@gmail.com>
2018-03-16 13:56:44 +0100

    Partialy added files, logic and controller for Search Flights.

---
+++ LowFlightFare/Controllers/SearchFlightsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LowFlightFare.Controllers
+{
+    public class SearchFlightsController : Controller
+    {
+        // GET: SearchFlights
+        public ActionResult SearchFlights()
+        {
+            return View();
+        }
+    }
+}
---
+++ LowFlightFare/ViewModels/SearchFlightsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LowFlightFare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LowFlightFare.ViewModels
+{
+    public class SearchFlightsViewModel
+    {
+        public SearchParameters SearchParameters { get; set; }
+        public SearchResults SearchResults { get; set; }
+    }
+}
