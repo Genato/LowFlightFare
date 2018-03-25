fc7400ef7dbc729906e302e660f9ac7402ed8ee4
Genato <druidhr@gmail.com>
2018-03-25 22:47:21 +0200

    Added reference PagedList.Mvc from nuget. Added view Results and it's ViewModel.

---
+++ LowFlightFare/ViewModels/ResultsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LowFlightFare.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LowFlightFare.ViewModels
+{
+    public class ResultsViewModel
+    {
+        //public List<SearchResults> SearchResults { get; set; }
+        public IPagedList<SearchResults> SearchResults { get; set; }
+    }
+}
