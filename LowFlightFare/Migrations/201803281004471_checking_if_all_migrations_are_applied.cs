1dcb0270746000b79ced264eeda198586e57a412
Genato <druidhr@gmail.com>
2018-03-28 12:06:07 +0200

    Added some minor changes to database tables

---
+++ LowFlightFare/Migrations/201803281004471_checking_if_all_migrations_are_applied.cs
@@ -0,0 +1,16 @@
+namespace LowFlightFare.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class checking_if_all_migrations_are_applied : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
